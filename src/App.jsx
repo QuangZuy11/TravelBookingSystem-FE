c44eeb840f4c80bdad6cf39bfa724488f4db91b3
DaiNho <98211436+DaiNho@users.noreply.github.com>
2025-09-24 18:12:46 +0700

    Updata
    Updata Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import TopBar from "./components/Topbar/Topbar";
+import Topbar from "./components/Topbar/Topbar";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 function App() {
   return (
-    <>
-      <TopBar />
+    <div className="app">
+      <Topbar />
       <Header />
       <HeroSection />
-    </>
+    </div>
   );
 }
 
