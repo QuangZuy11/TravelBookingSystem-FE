d3ed8dac6def61a8eb9f238bceda170014047b18
Pham Quang Duy <duypqhe176579@fpt.edu.vn>
2025-09-29 10:31:19 +0700

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import "./App.css";
 import Homepage from "./pages/traveler/Homepage";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomepageHotel from "./pages/traveler/HomepageHotel";
+// import HomepageHotel from "./pages/traveler/HomepageHotel";
 import AuthPage from "./pages/auth/AuthPage";
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
-    <Router>
-      <Routes>
-        {/* Trang homepage cho Traveler*/}
-        <Route path="/" element={<Homepage />} />
-        {/* Trang Hotel cho Traveler*/}
-        <Route path="/hotel" element={<HomepageHotel />} />
+      <Router>
+        <Routes>
+          {/* Trang homepage cho Traveler*/}
+          <Route path="/" element={<Homepage />} />
+          {/* Trang Hotel cho Traveler*/}
+          {/* <Route path="/hotel" element={<HomepageHotel />} /> */}
 
-        {/* Trang login */}
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
-    </Router>
+          {/* Trang login */}
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
