2cab84ec9f7359ada49eed29915f46462a3d502d
Pham Quang Duy <duypqhe176579@fpt.edu.vn>
2025-09-30 23:24:23 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HotelPage from "./pages/traveler/HotelPage";
 import AuthPage from "./pages/auth/AuthPage";
 import { AuthProvider } from './contexts/AuthContext';
+import Profile from "./pages/traveler/Profile";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
           <Route path="/hotel-page" element={<HotelPage />} />
           {/* Trang List Hotel cho Traveler*/}
           <Route path="/hotel-list" element={<HotelListPage />} />
+          {/* Trang List Hotel cho Traveler*/}
+          <Route path="/profile" element={< Profile />} />
 
           {/* Trang login */}
           <Route path="/auth" element={<AuthPage />} />
