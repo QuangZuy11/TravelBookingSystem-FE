cce18d23e3d8d2a2583b21776a6258fc4c141a71
truongbabiboi1204 <Caovantruong2503@gmail.com>
2025-09-30 21:51:45 +0700

    fe update profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Homepage from "./pages/traveler/Homepage";
-
+import Profile from "./pages/traveler/Profile";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import HomepageHotel from "./pages/traveler/HomepageHotel";
 import AuthPage from "./pages/auth/AuthPage";
 import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           {/* Trang Hotel cho Traveler*/}
           {/* <Route path="/hotel" element={<HomepageHotel />} /> */}
-
+          {/* Trang Profile cho Traveler */}
+          <Route path="/profile" element={<Profile />} />
           {/* Trang login */}
           <Route path="/auth" element={<AuthPage />} />
         </Routes>
