530466bf3e637c67a06ef8f1c44cdee66b0bd053
Pham Quang Duy <duypqhe176579@fpt.edu.vn>
2025-10-13 01:48:08 +0700

    Update link header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -104,7 +104,7 @@ const Header = () => {
       <nav className="header-nav">
         <a href="/">Trang Chủ</a>
         <a href="/tour">Tour Du Lịch</a>
-        <a href="/hotel-list">Khách Sạn</a>
+        <a href="/hotel-page">Khách Sạn</a>
         <a href="/about">Về Chúng Tôi</a>
         <a href="/contact">Liên Hệ</a>
       </nav>
