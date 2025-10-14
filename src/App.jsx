b302dfb07f742c0015dbe92e6f220f32b32dc277
leminhthanh9823 <thanhlmhe176868@fpt.edu.vn>
2025-10-14 19:09:16 +0700

    sửa lỗi admin ko vào được trang quản lý provider

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ function App() {
             <Route
               path="/admin/providers"
               element={
-                <ProtectedRoute requiredRole="ADMIN">
+                <ProtectedRoute requiredRole="Admin">
                   <PendingProvidersList />
                 </ProtectedRoute>
               }
